87140a3686efbc69393bdfe86958e9a5c3a3f68c
Minako Carino <irikiiomo11@gmail.com>
2020-07-29 09:39:19 +0000

    labda-1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,8 +27,10 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @post_url = "https://#{ENV['AWS_S3_UPLOAD_BUCKET_NAME']}.s3-#{ENV['AWS_REGION']}.amazonaws.com/store/" + @post.image_id + "-thumbnail."
     @comments = params[:likes_order].present? ? Comment.comment_like_ranks(@post.id) : @post.comments
     @comment = Comment.new
+    @image_url = "https://#{ENV['AWS_S3_UPLOAD_BUCKET_NAME']}.s3-#{ENV['AWS_REGION']}.amazonaws.com/store/"
     @tags = Post.tag_counts_on(:tags).order('count DESC')
     @post_like_ranks = Post.create_post_like_ranking
   end
