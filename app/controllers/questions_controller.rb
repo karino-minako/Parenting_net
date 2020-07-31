3a87655780fa9d498e38b41f753fbbdcd800ad75
Minako Carino <irikiiomo11@gmail.com>
2020-07-31 02:02:55 +0000

    Lambda5

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -28,7 +28,9 @@ class QuestionsController < ApplicationController
 
   def show
     @question = Question.find(params[:id])
-    @image_url = "https://#{ENV['AWS_S3_UPLOAD_BUCKET_NAME']}.s3-#{ENV['AWS_REGION']}.amazonaws.com/store/" + @question.image_id + "-thumbnail."
+    if @question.image_id != nil
+      @image_url = "https://#{ENV['AWS_S3_UPLOAD_BUCKET_NAME']}.s3-#{ENV['AWS_REGION']}.amazonaws.com/store/" + @question.image_id + "-thumbnail."
+    end
     @answers = params[:likes_order].present? ? Answer.answer_like_ranks(@question.id) : @question.answers
     @answer = Answer.new
     @tags = Question.tag_counts_on(:tags).order('count DESC')
