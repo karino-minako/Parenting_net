38b301e6161bbd25c63d31890245a22b49d1f84c
karino-minako <irikiiomo11@gmail.com>
2020-08-13 05:18:28 +0000

    rspec test fix

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Relationshipモデルのテスト', type: :model do
   describe 'アソシエーションのテスト' do
     context 'Userモデルとの関係' do
-      it '1:Nとなっている' do
+      it 'N:1となっている' do
         expect(Relationship.reflect_on_association(:follower).macro).to eq :belongs_to
         expect(Relationship.reflect_on_association(:followed).macro).to eq :belongs_to
       end
